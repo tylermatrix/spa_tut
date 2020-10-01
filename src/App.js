cf4d04fed43923d63630f3d1a09536dfa68762d1
tylermatrix <tylermaatrix@gmail.com>
2020-10-01 00:49:04 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Import React
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Meeting Log</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
